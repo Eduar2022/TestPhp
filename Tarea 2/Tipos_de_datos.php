7bd0984374fdb7d8927ca70921e24c8eabcd6cfa
Eduard Sanchez <eduar-sanchez02@hotmail.com>
2022-01-24 19:02:54 -0500

    EA-Test: Pruebas php

--- Tarea 2/Tipos_de_datos.php
+++ Tarea 2/Tipos_de_datos.php
@@ -6,6 +6,7 @@ $nombre = "Eduard"; //Cadena
 
 $estatura = 1.8; //Flotante o decimal
 
+
 $Programador = null; //Valor nulo
 
 echo "El nombre es ",$nombre." la edad es ".$Edad." su altura es ".$estatura." el puesto ".$Programador;
